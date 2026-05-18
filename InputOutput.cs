3a6710b2c723ba9975bae23d14dabaa00c9e3361
Nio <niegorka@yandex.ru>
2026-05-18 09:44:55 +0500

    Commit

---
+++ InputOutput.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+
+namespace Компилятор
+{
+    public struct TextPosition
+    {
+        public uint lineNumber;
+        public byte charNumber;
+        public TextPosition(uint ln = 0, byte c = 0)
+        {
+            lineNumber = ln;
+            charNumber = c;
+        }
+    }
+
+    public static class InputOutput
+    {
+        private static string[] _lines;
+        private static int _currentLineIdx;
+        private static int _currentCharPos;
+
+        public static char Ch { get; private set; }
+        public static TextPosition positionNow = new TextPosition(1, 0);
+        public static bool IsEndOfFile { get; private set; } = true;
+
+        public static void LoadFile(string filePath)
+        {
+            if (!File.Exists(filePath))
+                throw new FileNotFoundException("Файл не найден: " + filePath);
+            _lines = File.ReadAllLines(filePath);
+            if (_lines.Length == 0)
+            {
+                IsEndOfFile = true;
+                return;
+            }
+            _currentLineIdx = 0;
+            _currentCharPos = -1;
+            positionNow = new TextPosition(1, 0);
+            IsEndOfFile = false;
+            Ch = '\0';
+        }
+
+        public static void NextCh()
+        {
+            if (IsEndOfFile) return;
+
+            _currentCharPos++;
+            positionNow.charNumber = (byte)(_currentCharPos + 1);
+
+            if (_currentCharPos >= _lines[_currentLineIdx].Length)
+            {
+                _currentLineIdx++;
+                if (_currentLineIdx >= _lines.Length)
+                {
+                    IsEndOfFile = true;
+                    Ch = '\0';
+                    return;
+                }
+                _currentCharPos = -1;
+                positionNow.lineNumber = (uint)(_currentLineIdx + 1);
+                positionNow.charNumber = 0;
+                NextCh();
+                return;
+            }
+
+            Ch = _lines[_currentLineIdx][_currentCharPos];
+        }
+
+        public static string GetLine(int lineNumber) =>
+            (lineNumber >= 1 && lineNumber <= _lines.Length) 
+            ? _lines[lineNumber - 1] : "";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Компилятор
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("=== Тестирование модуля ввода-вывода ===\n");
+
+            string[] sourceLines = {
+                "program example ( input, output );",
+                "const c = 3;",
+                "b = 56;",
+                "var a : 'a' ... 'c';",
+                "k, i : integer;",
+                "begin",
+                "read( k, i );",
+                "for a := 'a' to 'c' do",
+                "case a of",
+                "    k: i := i * k;",
+                "    'b': i := i + 1;",
+                "    i : k := k + 2;",
+                "    b: i := i - k;",
+                "    c: i := ( i + k ) * 2",
+                "end;",
+                "writeln( i, k )",
+                "end."
+            };
+            File.WriteAllLines("example.pas", sourceLines);
+
+            var errors = new List<(int line, int col, byte code)>
+            {
+                (10, 5, 100),
+                (12, 5, 100),
+                (13, 5, 147),
+                (14, 5, 147)
+            };
+
+            const int LINE_WIDTH = 4;
+            int lineNum = 1;
+            int errCount = 0;
+
+            for (int i = 0; i < sourceLines.Length; i++)
+            {
+                Console.WriteLine($"{lineNum,LINE_WIDTH} {sourceLines[i]}");
+                lineNum++;
+
+                var lineErrors = errors.Where(e => e.line == i + 1).ToList();
+                if (lineErrors.Count == 0) continue;
+
+                foreach (var e in lineErrors)
+                {
+                    errCount++;
+                    Console.WriteLine($"**{errCount:D2}**");
+                    int arrowOffset = LINE_WIDTH + 1 + (e.col - 1);
+                    Console.WriteLine
+                        ($"{new string(' ', arrowOffset)}^ ошибка код {e.code}");
+                    string msg = e.code == 100
+                        ? "использование имени не соответствует описанию"
+                        : "тип метки не совпадает с типом выбирающего выражения";
+                    Console.WriteLine($"{new string(' ', LINE_WIDTH + 1)}{msg}");
+                }
+            }
+
+            Console.WriteLine($"\nКомпиляция окончена: ошибок — {errCount} !");
+            Console.ReadKey();
+        }
+    }
+}
